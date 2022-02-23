782ef53d220355f21e795e4b207ed01e4049299f
kid1493 <34921641+kid1493@users.noreply.github.com>
2022-02-23 16:01:22 +0900

    Create Wordcount1char.java

---
+++ self-training/Wordcount1char.java
@@ -0,0 +1,112 @@
+package ssafy;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class Wordcount1char {
+	/* 
+	Object, Text : input key-value pair type (always same (to get a line of input file))
+	Text, IntWritable : output key-value pair type
+	*/
+	public static class TokenizerMapper
+			extends Mapper<Object,Text,Text,IntWritable> {
+
+		// variable declairations
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		// map function (Context -> fixed parameter)
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+
+			// value.toString() : get a line
+			StringTokenizer itr = new StringTokenizer(value.toString());
+			while ( itr.hasMoreTokens() ) {
+				/*
+				 word.set(itr.nextToken());
+				 */
+				
+				String str = itr.nextToken().substring(0,1); // 단어의 맨 앞자리를 잘라서 str로 저장
+				char ch = str.charAt(0);	// 저장한 문자를 char 형으로 변형
+				if(ch >='A' && ch <= 'Z' || ch >= 'a' && ch <= 'z'){ // 변형한 char 문자열이 a~z / A~Z 사이일때만
+					word.set(str);	// 해당 문자를 word에 set
+					// emit a key-value pair
+					context.write(word,one); // 값 쌍을 저장
+				}
+				
+			}
+		}
+	}
+
+	/*
+	Text, IntWritable : input key type and the value type of input value list
+	Text, IntWritable : output key-value pair type
+	*/
+	public static class IntSumReducer
+			extends Reducer<Text,IntWritable,Text,IntWritable> {
+
+		// variables
+		private IntWritable result = new IntWritable();
+
+		// key : a disticnt word
+		// values :  Iterable type (data list)
+		public void reduce(Text key, Iterable<IntWritable> values, Context context) 
+				throws IOException, InterruptedException {
+
+			int sum = 0;
+			for ( IntWritable val : values ) {
+				sum += val.get();
+			}
+			result.set(sum);
+			context.write(key,result);
+		}
+	}
+
+
+	/* Main function */
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		String[] otherArgs = new GenericOptionsParser(conf,args).getRemainingArgs();
+		if ( otherArgs.length != 2 ) {
+			System.err.println("Usage: <in> <out>");
+			System.exit(2);
+		}
+		Job job = new Job(conf,"word count");
+		job.setJarByClass(Wordcount.class);
+
+		// let hadoop know my map and reduce classes
+		job.setMapperClass(TokenizerMapper.class);
+		job.setReducerClass(IntSumReducer.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		// set number of reduces
+		job.setNumReduceTasks(2);
+
+		// set input and output directories
+		FileInputFormat.addInputPath(job,new Path(otherArgs[0]));
+		FileOutputFormat.setOutputPath(job,new Path(otherArgs[1]));
+		System.exit(job.waitForCompletion(true) ? 0 : 1 );
+	}
+}
+
+
+
+
+
+/*
+hadoop을 이용한 실습 예제 wordcount (예제 파일을 읽고 사용된 단어의 빈도수를 정리) 를 분석하고 변형하여ㄴ
+추출하는 단어의 첫번째 알파벳만을 뽑아 빈도수를 정리하는 wordcount1char.java 파일으로 수정 실습 
+*/
