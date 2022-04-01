6e321fd63c3376a44d7692a990471a8def53d0fb
정해윤 <godbs98@naver.com>
2022-04-01 16:21:21 +0900

    Feat : add logo

--- frontend/src/layouts/full-layout/logo-light/LogoLight.vue
+++ frontend/src/layouts/full-layout/logo-light/LogoLight.vue
@@ -1,19 +1,22 @@
 <template>
   <b-navbar-brand >
        <!-- Logo icon -->
+       <router-link tag="a" to="/">
         <b class="logo-icon">
             <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
             <!-- Dark Logo icon -->
-            <img src="../../../assets/images/logo-light-icon.png" alt="homepage" >
+            <img src="../../../assets/images/site-logo.png" alt="homepage" >
             
         </b>
+    
         <!--End Logo icon -->
         <!-- Logo text -->
-        <span class="logo-text">
-            <!-- dark Logo text -->
-            <img src="../../../assets/images/logo-light-text.png" alt="homepage" >
+        <!-- <span class="logo-text">
+            dark Logo text -->
+            <!-- <img src="../../../assets/images/logo-light-text.png" alt="homepage" > -->
             
-        </span>
+        <!-- </span> --> 
+        </router-link>
   </b-navbar-brand>
 </template>
 
