e7b1e303b14f993568038f32b6d17f9667a70ffd
hanju <schw240@naver.com>
2022-04-08 02:19:52 +0900

    Fix: Cors 에러 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,22 @@
+// module.exports = {
+//   Proxy: {
+//     "/": {
+//       target: "http://j6a402.p.ssafy.io:8080/api",
+//       pathRewrite: { "^/": "" },
+//       changeOrigin: true,
+//     },
+//   },
+// };
 module.exports = {
-  publicPath: "/",
+  Server: {
+    proxy: {
+      "/": {
+        target: "https://nip.kdca.go.kr/",
+        pathRewrite: { "^/": "" },
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 };
+// http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey=Mo7exllHksroP%2BskN4M7XGw%2FLNhgZ3HNCV%2BZkPLkAJvPpGb21ckO%2F6KynGVAf4dMAbe%2F6a7COLv2g252FeE%2FFw%3D%3D&pageNo=1&numOfRows=1&startCreateDt=20220310
