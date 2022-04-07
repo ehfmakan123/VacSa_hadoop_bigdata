3110eb86a4b190f55e412afa9f93ccceef008449
ahnjiae <asj8839@naver.com>
2022-04-07 23:41:48 +0900

    Feat: 로고, 파비콘 수정
    - dark, light mode 설정
    - favicon 수정
    
    S06P22A402-93

--- frontend/src/layouts/full-layout/logo-dark/LogoDark.vue
+++ frontend/src/layouts/full-layout/logo-dark/LogoDark.vue
@@ -1,25 +1,24 @@
 <template>
   <b-navbar-brand href="#">
-       <!-- Logo icon -->
+    <!-- Logo icon -->
     <router-link tag="a" to="/">
-        <b class="logo-icon">
-            <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
-            <!-- Light Logo icon -->
-            <img src="../../../assets/images/site-logo.png" alt="homepage" >
-            
-        </b>
-        <!-- End Logo icon -->
-        <!-- Logo text -->
-        <!-- <span class="logo-text"> -->
-            <!-- Light Logo text -->
-           <!--  <img src="../../../assets/images/logo-text.png"  alt="homepage">
+      <b class="logo-icon">
+        <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
+        <!-- Light Logo icon -->
+        <img src="../../../assets/images/site-logo.png" alt="homepage" />
+      </b>
+      <!-- End Logo icon -->
+      <!-- Logo text -->
+      <!-- <span class="logo-text"> -->
+      <!-- Light Logo text -->
+      <!--  <img src="../../../assets/images/logo-text.png"  alt="homepage">
         </span> -->
-        </router-link>
+    </router-link>
   </b-navbar-brand>
 </template>
 
 <script>
 export default {
-    name: 'LogoDark',
-}
+  name: "LogoDark",
+};
 </script>
