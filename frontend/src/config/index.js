f70367a0381c1ab5f1a2e4954251b1f768be63af
DongKyu <aass0903@naver.com>
2022-04-08 10:57:39 +0900

    modify URL

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,4 +1,5 @@
 const API_BASE_URL = "https://j6a402.p.ssafy.io/api";
+// const API_BASE_URL = "https://localhost:8080/api";
 const VUE_APP_KAKAO_KEY = process.env.VUE_APP_KAKAO_KEY;
 
 module.exports = {
